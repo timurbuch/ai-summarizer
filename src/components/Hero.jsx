92b37a193462eca1268b1a09a89474e618ceaf77
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-25 10:30:07 +0200

    Add styles and copy to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
+import {logo} from "../assets"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="logo" className="w-28 object-contain"/>
+        <button type="button" onClick={() => window.open("https://github.com")} className="black_btn">Github</button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden"/>
+        <span className="orange_gradient"> OpenAI GPT-4</span>
+      </h1>
+    </header>
   )
 }
 
