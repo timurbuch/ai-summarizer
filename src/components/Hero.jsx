b5c7347694de0f6eacd4a6451e1f2214ee770ba5
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-25 14:45:34 +0200

    Update title, link to repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import {logo} from "../assets"
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt="logo" className="w-28 object-contain"/>
-        <button type="button" onClick={() => window.open("https://github.com")} className="black_btn">Github</button>
+        <button type="button" onClick={() => window.open("https://github.com/timurbuch/ai-summarizer")} className="black_btn">Github</button>
       </nav>
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden"/>
